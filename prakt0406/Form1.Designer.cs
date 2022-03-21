b6d936cf3f566ca45f2492cc3517dbfcaf9f0d3a
HellGoesOn <35145123+HellGoesOn@users.noreply.github.com>
2022-03-21 21:37:36 +0300

    Add project files.

---
+++ prakt0406/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace prakt0406
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.text_box_massiva = new System.Windows.Forms.RichTextBox();
+            this.text_box_minimalnogo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вывод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.text_box_massiva.Location = new System.Drawing.Point(58, 152);
+            this.text_box_massiva.Name = "richTextBox1";
+            this.text_box_massiva.Size = new System.Drawing.Size(152, 145);
+            this.text_box_massiva.TabIndex = 1;
+            this.text_box_massiva.Text = "";
+            // 
+            // textBox1
+            // 
+            this.text_box_minimalnogo.Location = new System.Drawing.Point(216, 152);
+            this.text_box_minimalnogo.Name = "textBox1";
+            this.text_box_minimalnogo.Size = new System.Drawing.Size(135, 23);
+            this.text_box_minimalnogo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Минимальное";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Массив";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_box_minimalnogo);
+            this.Controls.Add(this.text_box_massiva);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox text_box_massiva;
+        private System.Windows.Forms.TextBox text_box_minimalnogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ prakt0406/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prakt0406
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void random(float[] massiv)
+        {
+            Random rand = new Random();
+            for(int i  = 0; i < massiv.Length; i++)
+            {
+                massiv[i] = rand.Next(-20, 20);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float[] massiv = new float[10];
+            random(massiv);
+            float min = massiv[0];
+            for (int i= 0;i< massiv.Length;i++){
+                if(massiv[i] < min)
+                {
+                    min = massiv[i];
+                }
+                text_box_massiva.Text += massiv[i].ToString() + "\n";
+            }
+            text_box_minimalnogo.Text = min.ToString();
+
+        }
+    }
+}
